8d100babb53e3fe75c08970f8b35e0d18ed43c90
Misha <mishatambov@gmail.com>
2021-08-12 20:05:10 +0300

    create DB

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class LogInfo(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    workspace_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} {self.surname} {self.date} {self.workspace_number}"
