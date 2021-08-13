1ba721f6798a9ec07c703db28179d1b9d2755ff4
Misha <mishatambov@gmail.com>
2021-08-14 00:52:46 +0300

    made a conclusion without login

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
-# Create your views here.
+from app.models import LogInfo
+
+
+@method_decorator(login_required, name='dispatch')
+class index(ListView):
+    template_name = 'index.html'
+    model = LogInfo
+    context_object_name = 'records'
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST['name'], request.POST['surname'], request.POST['patronymic'], request.POST['workspace_number'])
+        return super().get(request, *args, **kwargs)
